c718ee64a6a950a8ece9d062d9c1693e096f85e9
isaacnwt <isaacnewton02@hotmail.com>
2021-07-15 18:29:45 -0300

    Teste de possibilidade de interface gráfica

---
+++ GUI/interface.py
@@ -0,0 +1,32 @@
+import PySimpleGUI as sg
+
+
+""" Interfaces """
+
+# configuração do tema da janela
+sg.theme('DarkAmber')
+
+# definição das abas
+aba_salas =   [ [ sg.Text('Gerenciamento de salas', size=(800,25)) ] ]
+
+aba_pessoas = [ [ sg.Text('Gerenciamento de pessoas') ] ]
+
+aba_sessoes = [ [ sg.Text('Gerenciamento de sessões') ] ]
+
+# conteúdo da janela
+layout = [  [sg.TabGroup([[
+                sg.Tab('Salas', aba_salas), 
+                sg.Tab('Pessoas', aba_pessoas), 
+                sg.Tab('Sessões', aba_sessoes)]])],
+            [sg.Button("Sair")]  ]
+
+# criação da janela
+janela = sg.Window('Cine Tão Tão Distante', layout, size=(1000, 500))
+
+# laço para processar EVENTOS e receber VALORES de entrada
+while True:
+    evento, valores = janela.read()
+    
+    # se o usuário fechar a tela ou clicar em SAIR
+    if evento == sg.WIN_CLOSED or evento == 'Sair':
+        break
